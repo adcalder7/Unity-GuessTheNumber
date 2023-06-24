67279c28c21cdb0466c6ed21704678f35692d3ac
Angel <adcalder7@gmail.com>
2023-06-24 18:03:17 -0400

    First commit
    First game created in Unity. Fun!

---
+++ Assets/Scripts/GuessTheNumber.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GuessTheNumber : MonoBehaviour {
+
+	public InputField input;
+	public Text infoText;
+	public Button resetButton;
+
+	public int from = 0;
+	public int to = 10;
+
+	private int guessNumber;
+	private int userGuess;
+
+	// Use this for initialization
+	void Start () {
+		Init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (string.IsNullOrEmpty (input.text))
+			return;
+		
+		var ugi = int.Parse (input.text);
+
+		if (ugi == guessNumber) {
+			infoText.text = "Bingo!";
+			input.interactable = false;
+			resetButton.interactable = true;
+		} else if (ugi < guessNumber) {
+			infoText.text = "Try a higher number";
+		} else if (ugi > guessNumber) {
+			infoText.text = "Try a lower number";
+		}
+	}
+
+	private void Init() {
+		infoText.text = "Enter a number between " + from + " and " + to;
+		input.text = "";
+		guessNumber = Random.Range (from, to);
+		input.interactable = true;
+		resetButton.interactable = false;
+	}
+
+	public void Reset() {
+		Init ();
+	}
+}
